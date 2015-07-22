622209c64d56a687c89d97aea0a70d83056726e0
J. Ryan Stinnett <jryans@gmail.com>
2015-07-22 18:12:16 -0500

    Added ESLint adjustments

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Uses "replace" from npm.
+
 set -ex
 
 hg mv browser/devtools devtools/client
@@ -7,3 +9,14 @@ hg mv toolkit/devtools/server devtools/server
 hg mv toolkit/devtools devtools/shared
 
 hg commit -m "Bug 912121 - Migrate major DevTools directories. r=bgrins"
+
+hg mv devtools/client/.eslintrc* devtools
+hg rm devtools/shared/.eslintrc
+
+replace '\"../../.eslintrc.mochitests\"' '"../../../.eslintrc.mochitests"' -r devtools
+replace '\"../../../../../browser/devtools/.eslintrc.mochitests\"' '"../../../.eslintrc.xpcshell"' -r devtools/server
+replace '\"../../../.eslintrc.xpcshell\"' '"../../../../.eslintrc.xpcshell"' -r devtools/client
+replace '\"../../../../../browser/devtools/.eslintrc.xpcshell\"' '"../../../.eslintrc.xpcshell"' -r devtools/server
+replace '\"../../../../browser/devtools/.eslintrc.xpcshell\"' '"../../../.eslintrc.xpcshell"' -r devtools/shared
+
+hg commit -m "Bug 912121 - Adjust ESLint files. r=bgrins"
