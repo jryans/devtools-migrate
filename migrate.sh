007dd4a8c57e5ad6a132f835e19525c6a7ee72b2
J. Ryan Stinnett <jryans@gmail.com>
2015-08-04 21:37:56 -0500

    Package DevTools client content in devtools.jar

--- migrate.sh
+++ migrate.sh
@@ -23,3 +23,18 @@ hg commit -m "Bug 912121 - Adjust ESLint files. r=bgrins"
 
 # hg export -o %m.patch
 hg import Bug_912121___Adjust_build_configs_and_test_manifests__r_glandium_bgrins.patch
+
+gsed -i -e 's/browser.jar/devtools.jar/' devtools/client/jar.mn
+gsed -i -e '/devtools.jar/a%   content devtools %content/' devtools/client/jar.mn
+replace 'content/browser/devtools/' 'content/' devtools/client/jar.mn
+replace 'chrome://browser/content/devtools/' 'chrome://devtools/content/' -r browser devtools
+hg commit -m "Bug 912121 - Package DevTools client content in devtools.jar
+
+Break DevTools content files out of browser.jar and move to a new DevTools
+specific jar.  Update all paths of the form:
+
+chrome://browser/content/devtools/
+
+to
+
+chrome://devtools/content/"
