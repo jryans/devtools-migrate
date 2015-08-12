586c6772d1ee9a6acabe2954dbd5d878699d1ef4
J. Ryan Stinnett <jryans@gmail.com>
2015-08-12 12:27:00 -0500

    Update require paths

--- rewrite-require.py
+++ rewrite-require.py
@@ -2,10 +2,38 @@
 
 import os
 import re
+import sys
 
-source_to_resource = dict()
+source_to_resource = {}
+resource_to_source = {}
+loader_map = {
+    "": "resource://gre/modules/commonjs/",
+    "main": "resource:///modules/devtools/main.js",
+    "definitions": "resource:///modules/devtools/definitions.js",
+    "devtools": "resource:///modules/devtools",
+    "devtools/toolkit": "resource://gre/modules/devtools",
+    "devtools/server": "resource://gre/modules/devtools/server",
+    "devtools/toolkit/webconsole": "resource://gre/modules/devtools/toolkit/webconsole",
+    "devtools/app-actor-front": "resource://gre/modules/devtools/app-actor-front.js",
+    "devtools/styleinspector/css-logic": "resource://gre/modules/devtools/styleinspector/css-logic",
+    "devtools/css-color": "resource://gre/modules/devtools/css-color",
+    "devtools/output-parser": "resource://gre/modules/devtools/output-parser",
+    "devtools/client": "resource://gre/modules/devtools/client",
+    "devtools/pretty-fast": "resource://gre/modules/devtools/pretty-fast.js",
+    "devtools/jsbeautify": "resource://gre/modules/devtools/jsbeautify/beautify.js",
+    "devtools/async-utils": "resource://gre/modules/devtools/async-utils",
+    "devtools/content-observer": "resource://gre/modules/devtools/content-observer",
+    "gcli": "resource://gre/modules/devtools/gcli",
+    "projecteditor": "resource:///modules/devtools/projecteditor",
+    "promise": "resource://gre/modules/Promise-backend.js",
+    "acorn": "resource://gre/modules/devtools/acorn",
+    "acorn/util/walk": "resource://gre/modules/devtools/acorn/walk.js",
+    "tern": "resource://gre/modules/devtools/tern",
+    "source-map": "resource://gre/modules/devtools/sourcemap/source-map.js",
+    "xpcshell-test": "resource://test"
+}
 
-def parse_moz_build(path):
+def record_source_to_resource(path):
     print("Reading %s" % path)
     is_client = path.split("/")[1] == "client"
     module_base = None
@@ -30,9 +58,63 @@ def parse_moz_build(path):
                 source = os.path.join(os.path.dirname(path), relative_source)
                 resource = module_base + "/" + os.path.basename(source)
                 source_to_resource[source] = resource
+                resource_to_source[resource] = source
                 print("%s -> %s" % (source, resource))
+
+resolve_map = list(loader_map.keys())
+resolve_map.sort(key = len, reverse = True)
+resolve_map = [[x, loader_map[x]] for x in resolve_map]
+# print(resolve_map)
+
+def resolve(id):
+    if id.startswith("resource://"):
+        return normalize_ext(id)
+    for id_base, resource_base in resolve_map:
+        if id.startswith(id_base):
+            return normalize_ext(id.replace(id_base, resource_base, 1))
+    raise RuntimeError("No mapping found for %s" % id)
+
+def normalize_ext(resource):
+    ext = os.path.splitext(resource)[1]
+    if ext:
+        return resource
+    return resource + '.js'
+
+def rewrite_require(path):
+    print("Updating %s" % path)
+    with open(path, 'r') as file:
+        for line in file:
+            require_match = re.search(r"require\([\"'](.*?)[\"']\)", line)
+            if not require_match:
+                continue
+            require_id = require_match.group(1)
+            print("Current: %s" % require_id)
+            resource = resolve(require_id)
+            print("Resource: %s" % resource)
+            if not "devtools" in resource:
+                continue
+            try:
+                source = resource_to_source[resource]
+            except KeyError:
+                if resource.startswith("resource:///"):
+                    resource = resource.replace("resource:///", "resource://gre/")
+                    source = resource_to_source[resource]
+                else:
+                    print("WARNING! No mapping for: %s" % resource)
+                    continue
+            base, ext = os.path.splitext(source)
+            if ext == ".js":
+                source = base
+            print("Updated: %s" % source)
 
 for root, dirs, files in os.walk("devtools"):
     for file in files:
         if file == "moz.build":
-            parse_moz_build(os.path.join(root, file))
+            record_source_to_resource(os.path.join(root, file))
+
+for root, dirs, files in os.walk("devtools"):
+    for file in files:
+        try:
+            rewrite_require(os.path.join(root, file))
+        except UnicodeDecodeError:
+            continue
